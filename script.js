f5ece7a354c58f8bab05e57e7a48e849c0013e2c
Crystal Koo <81462295+Crystal701@users.noreply.github.com>
2022-06-02 13:20:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const ul = document.querySelector("ul");
 const button = document.querySelector("button");
 const nav = document.querySelector("nav");
 const menu = document.querySelector("input");
+const header = document.querySelector("header");
 
 menu.addEventListener("change", () => {
     if (menu.checked) {
@@ -40,5 +41,34 @@ window.addEventListener("resize", () => {
     } else if (window.innerWidth > 800) {
         ul.style.display = "flex";
         button.style.display = "flex";
+    }
+})
+
+window.addEventListener("mousemove", () => {
+    if (window.event.clientY <= 100 && window.scrollY > 150) {
+        header.setAttribute("style", "display: flex; position: sticky; top: -150px; left: 0; transition: all 0.5s; transform: translateY(150px);")
+
+    } else if (window.event.clientY > 100 && window.scrollY > 150 && !document.querySelector(".main-nav")) {
+        header.setAttribute("style", "display: flex; transition: all 0.5s; transform: translateY(-150px);");
+    }
+})
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY < 150) {
+        header.setAttribute("style", "display: flex; position: sticky; top: 0; left: 0;");
+    } else {
+        header.setAttribute("style", "display: flex; transition: all 0.5s; transform: translateY(-150px)");
+    }
+
+    if (window.innerWidth <= 800 && menu.checked) {
+        ul.style.display = "none";
+        button.style.display = "none";
+
+        document.querySelector(".main-nav").remove();
+
+        nav.appendChild(ul);
+        nav.appendChild(button);
+
+        menu.checked = false;
     }
 })
